ccd7b6adad8bf7d260610d0054e95e16a9f24f17
Oliver Rose <hello@olivermrose.com>
2025-09-29 01:11:17 -0400

    add confirmation

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { ThemeProvider } from "next-themes";
 import { Inter } from "next/font/google";
 import Header from "@/components/Header";
+import { Toaster } from "@/components/ui/sonner";
 import "./globals.css";
 
 const inter = Inter({
@@ -27,6 +28,8 @@ export default function RootLayout({
 
 					{children}
 				</ThemeProvider>
+
+				<Toaster />
 			</body>
 		</html>
 	);
